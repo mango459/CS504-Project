8c91a20e950d0e95d764e9a494472bfed221955a
mango459 <158429833+mango459@users.noreply.github.com>
2024-10-25 21:24:55 -0400

    Initial Commit (#1)
    initial commit

---
+++ src/static.py
@@ -0,0 +1,13 @@
+"""This module defines static variables that may change from user to user. Define these here and do
+not push your changes. This file should be inlcuded in the .gitignore for the repos.
+
+For example:
+    DATA_DIR serves at the path on your machine in which the data for the project will be located.
+    Formatting this once here and then using this variable throughout the code will enalbe multiple
+    developers with completely different local file trees to run the same bit of code without having
+    to continually modify the actual code, you just modify it here and then import this variable to
+    the subsequent scripts.
+
+DONT PUSH THIS FILE ONCE YOU'VE FILLED IT OUT, ITS ON THE GITIGNORE BUT BE VIGILANT"""
+
+DATA_DIR: str = ""
