fcb1a5548825a36e760bc78413d89c0411e09792
mango459 <nczuzu767@gmail.com>
2024-11-11 12:23:40 -0500

    checkpoint work

--- get_data.py
+++ get_data.py
@@ -5,7 +5,7 @@ from src.data_pipeline import data_pipeline
 from src.static import DATA_DIR
 
 if __name__ == '__main__':
-    #config.update_static()
+    config.update_static()
     get_fhfa_data()
     loan_data, raw_loan_data = data_pipeline('loan')
     unit_data, raw_unit_data = data_pipeline('unit')
@@ -15,12 +15,12 @@ if __name__ == '__main__':
     raw_data = raw_data.drop_duplicates()
     if (dropped:=raw_data.shape[0] - nrows) > 0:
         print(f"raw_data: {dropped} duplicate rows dropped, {raw_data.shape[0]} remaining")
-    raw_data.to_csv(f'{DATA_DIR}/raw_data.csv')
+    raw_data.to_csv(f'{DATA_DIR}/raw_data.csv', index=False)
     mapped_data = unit_data.merge(loan_data, how='left',
                                    on=['record_number', 'enterprise_flag', 'year'])
     nrows = mapped_data.shape[0]
     mapped_data = mapped_data.drop_duplicates()
     if (dropped:=mapped_data.shape[0] - nrows) > 0:
         print(f"raw_data: {dropped} duplicate rows dropped, {mapped_data.shape[0]} remaining")
-    mapped_data.to_csv(f'{DATA_DIR}/mapped_data.csv')
+    mapped_data.to_csv(f'{DATA_DIR}/mapped_data.csv', index=False)
     print('data prep complete.')
